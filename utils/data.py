6921342ba3fdd748b1fa79b215c780d4105afbff
Wangsoo Kim <kws5078@nate.com>
2021-08-13 00:26:52 +0900

    Revision SQLiteConnector class

--- utils/data.py
+++ utils/data.py
@@ -1,3 +1,5 @@
+import sqlite3
+from typing import List
 from dataclasses import dataclass
 
 import jaydebeapi
@@ -9,26 +11,46 @@ class SQLiteConnector:
     database: str = '../easystock.sqlite'
 
     def __post_init__(self):
-        self.conn = jaydebeapi.connect("org.sqlite.JDBC",
-                                       f"""jdbc:sqlite:{self.database}""",
-                                       None,
-                                       "sqlite-jdbc-3.33.0.jar")
+        self.conn = sqlite3.connect(self.database)
+        #jaydebeapi.connect("org.sqlite.JDBC", f"""jdbc:sqlite:{self.database}""", None, "sqlite-jdbc-3.33.0.jar")
 
     def __del__(self):
         self.conn.close()
 
-    def select(self, query: str) -> pd.core.frame.DataFrame:
-        """SQL Select 문
+    def create(self, query: str) -> None:
+        """SQL CREATE 문
 
-        :param str query: SQL query
-
-        :returns pandas.core.frame.DataFrame: SQL query 결과
+        :param str query: create를 위한 query
         """
 
         cursor = self.conn.cursor()
         cursor.execute(query)
+        cursor.close()
+
+    def insert(self, query:str, data:List[tuple]) -> None:
+        """SQL INSERT 문
+
+        :param str query: insert를 위한 query
+        :param List[tuple] data: insert할 데이터
+        """
+
+        cursor = self.conn.cursor()
+        cursor.executemany(query, data)
+        self.conn.commit()
+
+    def select(self, query: str) -> pd.core.frame.DataFrame:
+        """SQL SELECT 문
+
+        :param str query: SQL query
+
+        :returns pandas.core.frame.DataFrame: SQL select query 결과
+        """
+
+        cursor = self.conn.cursor()
+        cursor.execute(query)
+        columns = [column[0] for column in cursor.description]
         data = cursor.fetchall()
-        df = pd.Dataframe(data)
+        df = pd.DataFrame(data, columns=columns)
         cursor.close()
 
         return df
